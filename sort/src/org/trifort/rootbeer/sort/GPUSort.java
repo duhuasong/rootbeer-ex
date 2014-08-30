e50389104c27b32cec4b5cd8510946103b64757c
Phil Pratt-Szeliga <pcpratts@trifort.org>
2014-08-30 18:46:43 -0400

    tuned version for mac gpu.

--- sort/src/org/trifort/rootbeer/sort/GPUSort.java
+++ sort/src/org/trifort/rootbeer/sort/GPUSort.java
@@ -8,61 +8,95 @@ import org.trifort.rootbeer.runtime.StatsRow;
 import org.trifort.rootbeer.runtime.CacheConfig;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Random;
 
 public class GPUSort {
 
-  private int[] newReversedArray(int size){
+  private int[] newArray(int size){
     int[] ret = new int[size];
 
     for(int i = 0; i < size; ++i){
-      ret[size-1-i] = i;
+      ret[i] = i;
     }
     return ret;
   }
 
+  public void checkSorted(int[] array, int outerIndex){
+    for(int index = 0; index < array.length; ++index){
+      if(array[index] != index){
+        for(int index2 = 0; index2 < array.length; ++index2){
+          System.out.println("array["+index2+"]: "+array[index2]);
+        }
+        throw new RuntimeException("not sorted: "+outerIndex);
+      }
+    }
+  }
+
+  public void fisherYates(int[] array)
+  {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--){
+      int index = random.nextInt(i + 1);
+      int a = array[index];
+      array[index] = array[i];
+      array[i] = a;
+    }
+  }
+
   public void sort(){
+    //should have 192 threads per SM
     int size = 2048;
     int sizeBy2 = size / 2;
     int numMultiProcessors = 2;
-    int blocksPerMultiProcessor = 1024;
+    int blocksPerMultiProcessor = 16;
     int outerCount = numMultiProcessors*blocksPerMultiProcessor;
     int[][] array = new int[outerCount][];
     for(int i = 0; i < outerCount; ++i){
-      array[i] = newReversedArray(size);
+      array[i] = newArray(size);
     }
 
-    GPUSortKernel kernel = new GPUSortKernel(array);
     Rootbeer rootbeer = new Rootbeer();
     List<GpuDevice> devices = rootbeer.getDevices();
     GpuDevice device0 = devices.get(0);
-    Context context0 = device0.createContext();
+    Context context0 = device0.createContext(263440);
     context0.setCacheConfig(CacheConfig.PREFER_SHARED);
-    ThreadConfig config0 = new ThreadConfig(sizeBy2, outerCount, sizeBy2);
-    long gpuStart = System.currentTimeMillis();
-    rootbeer.run(kernel, config0, context0);
-    long gpuStop = System.currentTimeMillis();
-    long gpuTime = gpuStop - gpuStart;
+    ThreadConfig config0 = new ThreadConfig(sizeBy2, outerCount,
+      (long) outerCount * (long) sizeBy2);
+    GPUSortKernel kernel = new GPUSortKernel(array);
 
-    List<StatsRow> stats = context0.getStats();
-    StatsRow row0 = stats.get(0);
-    System.out.println("serialization_time: "+row0.getSerializationTime());
-    System.out.println("execution_time: "+row0.getExecutionTime());
-    System.out.println("deserialization_time: "+row0.getDeserializationTime());
-    System.out.println("gpu_time: "+gpuTime);
+    while(true){
+      for(int i = 0; i < outerCount; ++i){
+        fisherYates(array[i]);
+      }
+      long gpuStart = System.currentTimeMillis();
+      rootbeer.run(kernel, config0, context0);
+      long gpuStop = System.currentTimeMillis();
+      long gpuTime = gpuStop - gpuStart;
 
-    for(int i = 0; i < outerCount; ++i){
-      array[i] = newReversedArray(size);
+      List<StatsRow> stats = context0.getStats();
+      StatsRow row0 = stats.get(0);
+      stats.clear();
+      System.out.println("serialization_time: "+row0.getSerializationTime());
+      System.out.println("execution_time: "+row0.getExecutionTime());
+      System.out.println("deserialization_time: "+row0.getDeserializationTime());
+      System.out.println("gpu_required_memory: "+context0.getRequiredMemory());
+      System.out.println("gpu_time: "+gpuTime);
+
+      for(int i = 0; i < outerCount; ++i){
+        checkSorted(array[i], i);
+        fisherYates(array[i]);
+      }
+
+      long cpuStart = System.currentTimeMillis();
+      for(int i = 0; i < outerCount; ++i){
+        Arrays.sort(array[i]);
+      }
+      long cpuStop = System.currentTimeMillis();
+      long cpuTime = cpuStop - cpuStart;
+      System.out.println("cpu_time: "+cpuTime);
+      double ratio = (double) cpuTime / (double) gpuTime;
+      System.out.println("ratio: "+ratio);
     }
-
-    long cpuStart = System.currentTimeMillis();
-    for(int i = 0; i < outerCount; ++i){
-      Arrays.sort(array[i]);
-    }
-    long cpuStop = System.currentTimeMillis();
-    long cpuTime = cpuStop - cpuStart;
-    System.out.println("cpu_time: "+cpuTime);
-    double ratio = (double) cpuTime / (double) gpuTime;
-    System.out.println("ratio: "+ratio);
   }
 
   public static void main(String[] args){
--- sort/src/org/trifort/rootbeer/sort/GPUSortKernel.java
+++ sort/src/org/trifort/rootbeer/sort/GPUSortKernel.java
@@ -19,30 +19,34 @@ public class GPUSortKernel implements Kernel {
     int index1b = index1a + 1;
     int index2a = index1a - 1;
     int index2b = index1a;
+    int index1a_shared = index1a << 2;
+    int index1b_shared = index1b << 2;
+    int index2a_shared = index2a << 2;
+    int index2b_shared = index2b << 2;
 
-    RootbeerGpu.setSharedInteger(index1a,array[index1a]);
-    RootbeerGpu.setSharedInteger(index1b,array[index1b]);
+    RootbeerGpu.setSharedInteger(index1a_shared,array[index1a]);
+    RootbeerGpu.setSharedInteger(index1b_shared,array[index1b]);
     //outer pass
-    int arrayLength = array.length;
+    int arrayLength = array.length >> 1;
     for(int i = 0; i < arrayLength; ++i){
-      int value1 = RootbeerGpu.getSharedInteger(index1a);
-      int value2 = RootbeerGpu.getSharedInteger(index1b);
+      int value1 = RootbeerGpu.getSharedInteger(index1a_shared);
+      int value2 = RootbeerGpu.getSharedInteger(index1b_shared);
       if(value2 < value1){
-        RootbeerGpu.setSharedInteger(index1a, value2);
-        RootbeerGpu.setSharedInteger(index1b, value1);
+        RootbeerGpu.setSharedInteger(index1a_shared, value2);
+        RootbeerGpu.setSharedInteger(index1b_shared, value1);
       }
       RootbeerGpu.syncthreads();
       if(index2a >= 0){
-        value1 = RootbeerGpu.getSharedInteger(index2a);
-        value2 = RootbeerGpu.getSharedInteger(index2b);
+        value1 = RootbeerGpu.getSharedInteger(index2a_shared);
+        value2 = RootbeerGpu.getSharedInteger(index2b_shared);
         if(value2 < value1){
-          RootbeerGpu.setSharedInteger(index2a, value2);
-          RootbeerGpu.setSharedInteger(index2b, value1);
+          RootbeerGpu.setSharedInteger(index2a_shared, value2);
+          RootbeerGpu.setSharedInteger(index2b_shared, value1);
         }
       }
       RootbeerGpu.syncthreads();
     }
-    array[index1a] = RootbeerGpu.getSharedInteger(index1a);
-    array[index1b] = RootbeerGpu.getSharedInteger(index1b);
+    array[index1a] = RootbeerGpu.getSharedInteger(index1a_shared);
+    array[index1b] = RootbeerGpu.getSharedInteger(index1b_shared);
   }
 }
