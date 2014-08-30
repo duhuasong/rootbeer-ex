f8fb95ebb5043864990d8a4fdccf565f9ef11fec
Phil Pratt-Szeliga <pcpratts@trifort.org>
2014-08-30 11:29:33 -0400

    showing cpu timing

--- sort/src/org/trifort/rootbeer/sort/GPUSort.java
+++ sort/src/org/trifort/rootbeer/sort/GPUSort.java
@@ -4,7 +4,9 @@ import org.trifort.rootbeer.runtime.Rootbeer;
 import org.trifort.rootbeer.runtime.GpuDevice;
 import org.trifort.rootbeer.runtime.Context;
 import org.trifort.rootbeer.runtime.ThreadConfig;
+import org.trifort.rootbeer.runtime.StatsRow;
 import java.util.List;
+import java.util.Arrays;
 
 public class GPUSort {
 
@@ -17,17 +19,27 @@ public class GPUSort {
     }
 
     GPUSortKernel kernel = new GPUSortKernel(array);
-
     Rootbeer rootbeer = new Rootbeer();
     List<GpuDevice> devices = rootbeer.getDevices();
     GpuDevice device0 = devices.get(0);
-    Context context0 = device0.createContext(1024);
+    Context context0 = device0.createContext(32+32+(sizeBy2*4));
     ThreadConfig config0 = new ThreadConfig(sizeBy2, 1, sizeBy2);
     rootbeer.run(kernel, config0, context0);
 
-    for(int value : array){
-      System.out.println("value: "+value);
+    List<StatsRow> stats = context0.getStats();
+    StatsRow row0 = stats.get(0);
+    System.out.println("serialization_time: "+row0.getSerializationTime());
+    System.out.println("execution_time: "+row0.getExecutionTime());
+    System.out.println("deserialization_time: "+row0.getDeserializationTime());
+
+    for(int i = 0; i < size; ++i){
+      array[size-1-i] = i;
     }
+    long cpuStart = System.currentTimeMillis();
+    Arrays.sort(array);
+    long cpuStop = System.currentTimeMillis();
+    long cpuTime = cpuStop - cpuStart;
+    System.out.println("cpu_time: "+cpuTime);
   }
 
   public static void main(String[] args){
--- sort/src/org/trifort/rootbeer/sort/GPUSortKernel.java
+++ sort/src/org/trifort/rootbeer/sort/GPUSortKernel.java
@@ -30,7 +30,7 @@ public class GPUSortKernel implements Kernel {
         RootbeerGpu.setSharedInteger(4*index1b, value1);
       }
       RootbeerGpu.syncthreads();
-      if(index2a > 0){
+      if(index2a >= 0){
         value1 = RootbeerGpu.getSharedInteger(4*index2a);
         value2 = RootbeerGpu.getSharedInteger(4*index2b);
         if(value2 < value1){
@@ -44,41 +44,3 @@ public class GPUSortKernel implements Kernel {
     array[index1b] = RootbeerGpu.getSharedInteger(4*index1b);
   }
 }
-
-/*
-template<std::size_t bound,
-         std::size_t grainsize,
-         typename RandomAccessIterator1,
-         typename RandomAccessIterator2,
-         typename Compare>
-__forceinline__ __device__
-void stable_odd_even_transpose_sort_by_key(const bounded<bound,agent<grainsize> > &,
-                                           RandomAccessIterator1 keys_first, RandomAccessIterator1 keys_last,
-                                           RandomAccessIterator2 values_first,
-                                           Compare comp)
-{
-  stable_odd_even_transpose_sort_by_key_impl<0, bound>::sort(keys_first, values_first, keys_last - keys_first, comp);
-} // end stable_odd_even_transpose_sort_by_key()
-
-template<int i, int bound>
-struct stable_odd_even_transpose_sort_by_key_impl
-{
-  template<typename RandomAccessIterator1, typename RandomAccessIterator2, typename Compare>
-  static __device__
-  void sort(RandomAccessIterator1 keys, RandomAccessIterator2 values, int n, Compare comp)
-  {
-    for(int j = 1 & i; j < bound - 1; j += 2)
-    {
-      if(j + 1 < n && comp(keys[j + 1], keys[j]))
-      {
-        using thrust::swap;
-
-      	swap(keys[j], keys[j + 1]);
-      	swap(values[j], values[j + 1]);
-      }
-    }
-
-    stable_odd_even_transpose_sort_by_key_impl<i + 1, bound>::sort(keys, values, n, comp);
-  }
-};
-*/
