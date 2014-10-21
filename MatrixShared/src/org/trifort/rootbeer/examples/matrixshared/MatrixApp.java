a001e4f63d67651fe1b42607bc33ccf3b3cfe5a3
Phil Pratt-Szeliga <pcpratts@trifort.org>
2014-10-21 14:17:48 -0400

    matrix shared example working with 2d thread_indexes

--- MatrixShared/src/org/trifort/rootbeer/examples/matrixshared/MatrixApp.java
+++ MatrixShared/src/org/trifort/rootbeer/examples/matrixshared/MatrixApp.java
@@ -2,6 +2,9 @@ package org.trifort.rootbeer.examples.matrixshared;
 
 import org.trifort.rootbeer.runtime.util.Stopwatch;
 import org.trifort.rootbeer.runtime.Rootbeer;
+import org.trifort.rootbeer.runtime.Context;
+import org.trifort.rootbeer.runtime.GpuDevice;
+import org.trifort.rootbeer.runtime.CacheConfig;
 import org.trifort.rootbeer.runtime.Kernel;
 import org.trifort.rootbeer.runtime.StatsRow;
 import org.trifort.rootbeer.runtime.ThreadConfig;
@@ -10,36 +13,36 @@ import java.util.ArrayList;
 
 public class MatrixApp {
 
-  private float[][] matrixA;
-  private float[][] matrixB;
-  private float[][] matrixCGPU;
-  private float[][] matrixCCPU;
+  private float[] matrixA;
+  private float[] matrixB;
+  private float[] matrixCGPU;
+  private float[] matrixCCPU;
   private int dimA;
   private int dimB;
   private Context context;
 
   private void setup(){
-    dimA = 1024;
-    dimB = 1024;
+    dimA = 32;
+    dimB = 32;
 
-    matrixA = new float[dimA][dimB];
-    matrixB = new float[dimA][dimB];
-    matrixCGPU = new float[dimA][dimB];
-    matrixCCPU = new float[dimA][dimB];
+    matrixA = new float[dimA*dimB];
+    matrixB = new float[dimA*dimB];
+    matrixCGPU = new float[dimA*dimB];
+    matrixCCPU = new float[dimA*dimB];
 
     Rootbeer rootbeer = new Rootbeer();
     List<GpuDevice> devices = rootbeer.getDevices();
     GpuDevice device0 = devices.get(0);
     context = device0.createContext();
-    context.setCacheConfig(CacheConfig.PREFER_L1);
-    context.setThreadConfig(dimA, dimB, dimA * dimB);
+    context.setCacheConfig(CacheConfig.PREFER_SHARED);
+    context.setThreadConfig(dimA, dimA, dimB, dimB, dimA * dimB);
     context.setKernel(new MatrixKernel(matrixA, matrixB, matrixCGPU));
     context.buildState();
 
     for(int i = 0; i < dimA; ++i){
       for(int j = 0; j < dimB; ++j){
-        matrixA[i][j] = i * dimB + j;
-        matrixB[i][j] = i * dimB + j;
+        matrixA[i * dimB + j] = i * dimB + j;
+        matrixB[i * dimB + j] = i * dimB + j;
       }
     }
   }
@@ -47,8 +50,8 @@ public class MatrixApp {
   private void init(){
     for(int i = 0; i < dimA; ++i){
       for(int j = 0; j < dimB; ++j){
-        matrixCGPU[i][j] = 0;
-        matrixCCPU[i][j] = 0;
+        matrixCGPU[i * dimB + j] = 0;
+        matrixCCPU[i * dimB + j] = 0;
       }
     }
   }
@@ -59,9 +62,9 @@ public class MatrixApp {
       for(int j = 0; j < dimB; ++j){
         float sum = 0;
         for(int k = 0; k < dimA; ++k){
-          sum += matrixA[i][k] * matrixB[k][j];
+          sum += matrixA[i * dimB + k] * matrixB[k * dimB + j];
         }
-        matrixCGPU[i][j] = sum;
+        matrixCGPU[i * dimB + j] = sum;
       }
     }
     long stopTime = System.currentTimeMillis();
--- MatrixShared/src/org/trifort/rootbeer/examples/matrixshared/MatrixKernel.java
+++ MatrixShared/src/org/trifort/rootbeer/examples/matrixshared/MatrixKernel.java
@@ -8,36 +8,63 @@ import java.util.ArrayList;
 
 public class MatrixKernel implements Kernel {
 
-  private float[][] matrixA;
-  private float[][] matrixB;
-  private float[][] matrixCGPU;
-  private const int FLOAT_SIZE = 4;
-  private const int TILE_WIDTH = 32;
-  private const int SUBMATRIX_SIZE = TILE_WIDTH*TILE_WIDTH;
-  private int numSubmatrices;
+  private float[] matrixA;
+  private float[] matrixB;
+  private float[] matrixCGPU;
+  private final static int FLOAT_SIZE = 4;
+  private final static int BLOCK_SIZE = 32;
+  private final static int wA = 32;
+  private final static int wB = 32;
 
-  public MatrixKernel(float[][] matrixA, float[][] matrixB, float[][] matrixCGPU){
+  public MatrixKernel(float[] matrixA, float[] matrixB, float[] matrixCGPU){
     this.matrixA = matrixA;
     this.matrixB = matrixB;
     this.matrixCGPU = matrixCGPU;
-
-    numSubmatrices = matrixA.length = SUBMATRIX_SIZE;
   }
 
   public void gpuMethod(){
-    int thread_idxx = RootbeerGpu.getThreadIdxx();
-    int block_idxx = RootbeerGpu.getBlockIdxx();
-    int dim = RootbeerGpu.getBlockDimx();
+    int bx = RootbeerGpu.getBlockIdxx();
+    int by = RootbeerGpu.getBlockIdxy();
+    int tx = RootbeerGpu.getThreadIdxx();
+    int ty = RootbeerGpu.getThreadIdxy();
 
-    int localnumSubmatrices = numSubmatrices;
-    for(int i = 0; i < localnumSubmatrices; ++i){
-      int matrixARow = (i * TILE_WIDTH) + thread_idxx;
+    int aBegin = wA * BLOCK_SIZE * by;
+    int aEnd = aBegin + wA - 1;
+    int aStep = BLOCK_SIZE;
+    int bBegin = BLOCK_SIZE * bx;
+    int bStep  = BLOCK_SIZE * wB;
+    float Csub = 0;
+
+    for (int a = aBegin, b = bBegin;
+         a <= aEnd;
+         a += aStep, b += bStep)
+    {
+
+      float aValue = matrixA[a + wA * ty + tx];
+      float bValue = matrixB[b + wB * ty + tx];
+
+      int sharedArrayIndex = ty * BLOCK_SIZE + tx;
+      int aStart = 0;
+      int bStart = BLOCK_SIZE * BLOCK_SIZE;
+      int aIndex = aStart + sharedArrayIndex;
+      int bIndex = bStart + sharedArrayIndex;
+
+      RootbeerGpu.setSharedFloat(aIndex * FLOAT_SIZE, aValue);
+      RootbeerGpu.setSharedFloat(bIndex * FLOAT_SIZE, bValue);
+
+      RootbeerGpu.syncthreads();
+
+      for (int k = 0; k < BLOCK_SIZE; ++k)
+      {
+        aValue = RootbeerGpu.getSharedFloat((aStart + (ty * BLOCK_SIZE + k)) * FLOAT_SIZE);
+        bValue = RootbeerGpu.getSharedFloat((bStart + (k * BLOCK_SIZE + tx)) * FLOAT_SIZE);
+        Csub += aValue * bValue;
+      }
+
+      RootbeerGpu.syncthreads();
     }
 
-    float sum = 0;
-    for(int k = 0; k < dim; ++k){
-      sum += matrixA[block_idxx][k] * matrixB[k][thread_idxx];
-    }
-    matrixCGPU[block_idxx][thread_idxx] = sum;
+    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+    matrixCGPU[c + wB * ty + tx] = Csub;
   }
 }
