cb976fe7167647f5fca0488bd491b92f8dbccb11
Phil Pratt-Szeliga <pcpratts@trifort.org>
2014-10-26 11:33:22 -0400

    working more on hmm-gpu

--- hmm-gpu/src/org/trifort/rootbeer/examples/hmm/Main.java
+++ hmm-gpu/src/org/trifort/rootbeer/examples/hmm/Main.java
@@ -1,17 +1,36 @@
 package org.trifort.rootbeer.examples.hmm;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import be.ac.ulg.montefiore.run.jahmm.io.HmmBinaryWriter;
+import be.ac.ulg.montefiore.run.jahmm.io.HmmBinaryReader;
+import be.ac.ulg.montefiore.run.jahmm.Hmm;
+import be.ac.ulg.montefiore.run.jahmm.ObservationInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.File;
+import org.apache.commons.io.IOUtils;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
   private Hmm openHmm(String localFilename){
     try {
-      FileInputStream stream = new FileInputStream(saved_hmm);
-      hmm = HmmBinaryReader.read(stream);
+      FileInputStream stream = new FileInputStream(localFilename);
+      Hmm hmm = HmmBinaryReader.read(stream);
       stream.close();
       return hmm;
     } catch(Exception ex){
       ex.printStackTrace();
       return null;
     }
+  }
+
+  private Hmm createHmm(){
+    return null;
   }
 
   private boolean remoteFileExists(String remoteFile){
@@ -27,6 +46,43 @@ public class Main {
     }
   }
 
+  private void downloadFile(String remoteFilename, String localFilename){
+    System.out.println("downloading: "+remoteFilename);
+    try {
+      URL url = new URL(remoteFilename);
+      OutputStream outputStream = new FileOutputStream(localFilename);
+      InputStream inputStream = url.openStream();
+
+      IOUtils.copy(inputStream, outputStream);
+      inputStream.close();
+      outputStream.close();
+    } catch(Exception ex){
+      ex.printStackTrace();
+    }
+  }
+
+  private List<File> getMovieFiles(){
+    File sourceFolder = new File("Mutant_Fbodies/");
+    File[] children = sourceFolder.listFiles();
+    List<File> ret = new ArrayList<File>();
+    for(File child : children){
+      if(child.isDirectory()){
+        File[] children2 = child.listFiles();
+        for(File child2 : children2){
+          if(child2.isDirectory()){
+            String name = child2.getName();
+            if(name.startsWith(".")){
+              continue;
+            }
+
+            ret.add(child2);
+          }
+        }
+      }
+    }
+    return ret;
+  }
+
   public void runTest(boolean test1){
     Hmm hmm = null;
     int numStates = 8160;
@@ -35,9 +91,9 @@ public class Main {
     if(localFile.exists()){
       hmm = openHmm(localFilename);
     } else {
-      String remoteFile = "http://trifort.org/hmm/"+localFilename;
-      if(remoteFileExists(remoteFile)){
-        downloadFile(remoteFile, localFile);
+      String remoteFilename = "http://trifort.org/hmm/"+localFilename;
+      if(remoteFileExists(remoteFilename)){
+        downloadFile(remoteFilename, localFilename);
         hmm = openHmm(localFilename);
       } else {
         hmm = createHmm();
@@ -51,13 +107,29 @@ public class Main {
       }
     }
 
+    List<File> movieFiles = getMovieFiles();
+
     if(test1){
+      File file1 = movieFiles.get(11);
+      OfCoarseMovie movie = new OfCoarseMovie(file1);
+      List<ObservationInteger> signal1 = movie.getSignal();
+
       ForwardBackwardScaledGpuInt forwardBackward =
-          new ForwardBackwardScaledGpuInt(convert(full_signal.get(0)), new HmmGpuInt(hmm));
+          new ForwardBackwardScaledGpuInt(convert(signal1), new HmmGpuInt(hmm));
       double prob = forwardBackward.probability();
     } else {
-      
+
     }
+  }
+
+  private int[] convert(List<ObservationInteger> signal){
+    int[] ret = new int[signal.size()];
+    int index = 0;
+    for(ObservationInteger item : signal){
+      ret[index] = item.value;
+      ++index;
+    }
+    return ret;
   }
 
   private void showUsage(){
@@ -83,7 +155,7 @@ public class Main {
   }
 
 
-  public static main(String[] args){
+  public static void main(String[] args){
     Main main = new Main();
     try {
       boolean test1 = main.parseArgs(args);
---
+++ hmm-gpu/src/org/trifort/rootbeer/examples/hmm/OfCoarseMovie.java
@@ -0,0 +1,17 @@
+package org.trifort.rootbeer.examples.hmm;
+
+import java.io.File;
+import java.util.List;
+import be.ac.ulg.montefiore.run.jahmm.ObservationInteger;
+
+public class OfCoarseMovie {
+
+
+  public OfCoarseMovie(File baseFolder){
+
+  }
+
+  public List<ObservationInteger> getSignal(){
+    return null;
+  }
+}
