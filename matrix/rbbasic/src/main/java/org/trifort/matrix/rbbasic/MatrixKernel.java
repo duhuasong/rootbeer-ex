21d5821b894fe07a2417f68ff7ea3ad73cd818b4
Phil Pratt-Szeliga <pcpratts@trifort.org>
2015-01-06 10:44:47 -0500

    working on matrix examples

--- matrix/rbbasic/src/main/java/org/trifort/matrix/rbbasic/MatrixKernel.java
+++ matrix/rbbasic/src/main/java/org/trifort/matrix/rbbasic/MatrixKernel.java
@@ -1,5 +1,35 @@
 package org.trifort.matrix.rbbasic;
 
+import org.trifort.rootbeer.runtime.Kernel;
+import org.trifort.rootbeer.runtime.RootbeerGpu;
+
 public class MatrixKernel implements Kernel {
 
+  private double[][] a;
+  private double[][] b;
+  private double[][] c;
+  
+  public MatrixKernel(double[][] a, double[][] b, double[][] c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  
+  @Override
+  public void gpuMethod() {
+    double[][] registerA = a;
+    double[][] registerB = b;
+    double[][] registerC = c;
+    int size = registerA.length;
+    
+    int threadId = RootbeerGpu.getThreadId();
+    int i = threadId / size;
+    int j = threadId % size;
+
+    double sum = 0;
+    for(int k = 0; k < size; ++k){
+      sum += registerA[i][k] * registerB[k][j];
+    }
+    registerC[i][j] = sum;
+  }
 }
